38c816df4db35a54e7b6cfee7e313ab409708da7
Kalliojumala <joel.kallio@gmail.com>
2022-04-05 13:55:03 +0300

    post/get

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
-require('dotenv').config()
+require("dotenv").config();
 const express = require("express");
 const app = express();
 app.use(express.json());
-app.use(express.static('build'));
-
+app.use(express.static("build"));
 
 //Morgan config, longer "logs" on POST- reqs
 const morgan = require("morgan");
@@ -29,34 +28,30 @@ app.use(
 );
 
 //CORS for dev frontend connection
-const cors = require('cors')
+const cors = require("cors");
 app.use(cors());
 
 //Module "Person"
-const Person = require('./modules/personSchema');
-
-
+const Person = require("./modules/personSchema");
 
 //Get request, all data.
 app.get("/api/persons", (request, response) => {
   Person.find({}).then((result) => {
     response.json(result);
-  })
-  
+  });
 });
 
 //Get single entry
 app.get("/api/persons/:id", (request, response) => {
   const id = Number(request.params.id);
-  Person.find({id: id}).then((result) => {
+  Person.find({ id: id }).then((result) => {
     console.log(result.length);
     if (result.length > 0) {
       response.json(result);
     } else {
       response.status(404).end();
-    } 
-  })
-   
+    }
+  });
 });
 
 //Delete entry
@@ -68,38 +63,48 @@ app.delete("/api/persons/:id", (request, response) => {
 });
 
 app.post("/api/persons", (request, response) => {
-  if (numbers.find((item) => item.name === request.body.name)) {
-    return response
-      .status(400)
-      .json({ error: "Number/Person already added! Name must be unique." });
-  }
-  if (!request.body.name || !request.body.phonenumber) {
-    console.log(!request.body.phonenumber, request.body.phonenumber)
+  if (!request.body.name || !request.body.number) {
     return response
       .status(400)
       .json({ error: "Pass name and number with your request!" });
   }
 
-  const newId = Math.floor(Math.random() * 20000);
-  let newNumber = { ...request.body, id: newId };
-  numbers = numbers.concat(newNumber);
-  response.json(newNumber);
+  Person.find({ name: request.body.name }).then((result) => {
+    let nameComparison = result.length > 0 ? result[0].name : "Okay";
+    if (nameComparison === request.body.name) {
+      return response
+        .status(400)
+        .json({ error: "Number/Person already added! Name must be unique." });
+    } else {
+      const newPerson = new Person({
+        name: request.body.name,
+        number: request.body.number,
+        id: Math.floor(Math.random() * 20000),
+      });
+
+      newPerson.save().then((result) => {
+        console.log("Saved new entry!", result);
+        response.json(newPerson);
+      });
+    }
+  });
 });
 
 //Get info page (date, no of entries/numbers)
 app.get("/info", (request, response) => {
   let numberOfEntries = 0;
-  Person.find({}).then((result) => {
-    console.log(result)
-    result.forEach(item => {
-      numberOfEntries += 1
-      
+  Person.find({})
+    .then((result) => {
+      console.log(result);
+      result.forEach((item) => {
+        numberOfEntries += 1;
+      });
     })
-  }).then(() => {
-  let requestDateTime = new Date();
-  const info = `<div>Phonebook has ${numberOfEntries} entries</div><div>${requestDateTime}</div>`
-  response.send(info);
-  })
+    .then(() => {
+      let requestDateTime = new Date();
+      const info = `<div>Phonebook has ${numberOfEntries} entries</div><div>${requestDateTime}</div>`;
+      response.send(info);
+    });
 });
 
 const PORT = process.env.PORT || 3001;
